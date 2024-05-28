8bb016eaad79c6271c91dbf59ddd7c0e75c690d6
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-05-28 23:59:56 +0200

    add app.py to create a GUI

---
+++ app.py
@@ -0,0 +1,72 @@
+import wx
+import os
+
+
+class MainWindow(wx.Frame):
+    def __init__(self, parent, title):
+        self.dirname = ''
+
+        # A "-1" in the size parameter instructs wxWidgets to use the default size.
+        # In this case, we select 200px width and the default height.
+        wx.Frame.__init__(self, parent, title=title, size=(200, 200))
+        self.SetIcon(wx.Icon('Data/chart_icon.ico'))
+
+        self.control = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)x
+
+        self.CreateStatusBar()  # A Statusbar in the bottom of the window
+
+        # Setting up the menu.
+        filemenu = wx.Menu()
+        menuOpen = filemenu.Append(wx.ID_OPEN, "&Open", " Open a file to edit")
+        menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program")
+        menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", " Terminate the program")
+
+        # Creating the menubar.
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu, "&File")  # Adding the "filemenu" to the MenuBar
+        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
+
+        # Events.
+        self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+        self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
+        self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
+
+        # Use some sizers to see layout options
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        self.sizer.Add(self.control, 1, wx.ALIGN_CENTER)
+
+        # Layout sizers
+        # self.SetSizer(self.sizer)
+        # self.SetAutoLayout(1)
+        # self.sizer.Fit(self)
+        self.Show()
+
+    def OnAbout(self, e):
+        # Create a message dialog box
+        dlg = wx.MessageDialog(self, 'By Petrus Kirsten', 'About Rheometer Plots', wx.OK)
+        dlg.ShowModal()  # Shows it
+        dlg.Destroy()  # finally destroy it when finished.
+
+    def OnExit(self, e):
+        self.Close(True)  # Close the frame.
+
+    def OnOpen(self, e):
+        """ Open a file"""
+        dlg = wx.FileDialog(self, 'Select the data', self.dirname, '', '*.*', wx.FD_OPEN)
+        if dlg.ShowModal() == wx.ID_OK:
+            self.filename = dlg.GetFilename()
+            self.dirname = dlg.GetDirectory()
+            f = open(os.path.join(self.dirname, self.filename), 'r')
+            self.control.SetValue(f.read())
+            f.close()
+        dlg.Destroy()
+
+
+def main() -> None:
+    app = wx.App(False)
+    frame = MainWindow(None, 'Rheometer Plots').Center()
+    app.MainLoop()
+
+
+if __name__ == "__main__":
+    main()
--- total.py
+++ total.py
@@ -5,81 +5,81 @@ from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
 warnings.simplefilter('ignore')
 
+if __name__ == "__main__":
+    def argconfig():
+        # Parse command line arguments
+        parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
 
-def argconfig():
-    # Parse command line arguments
-    parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
+        # Argumentos to DynamicCompression.__init__
+        parser.add_argument(
+            '-p', '--datapath', type=str,
+            help='Datafile path.')
+        parser.add_argument(
+            '-np', '--n_points', default=196, type=int,
+            help='Numbers of points per period.')
+        parser.add_argument(
+            '-fs', '--fig_size', default=(34, 14), type=tuple,
+            help='Size of the figure chart.')
 
-    # Argumentos to DynamicCompression.__init__
-    parser.add_argument(
-        '-p', '--datapath', type=str,
-        help='Datafile path.')
-    parser.add_argument(
-        '-np', '--n_points', default=196, type=int,
-        help='Numbers of points per period.')
-    parser.add_argument(
-        '-fs', '--fig_size', default=(34, 14), type=tuple,
-        help='Size of the figure chart.')
+        # Argumentos to DynamicCompression.total_plot
+        parser.add_argument(
+            '-ns', '--normal_wave', default=False, type=bool,
+            help='If True, plot fitted sine wave from height oscillation.')
+        parser.add_argument(
+            '-ds', '--damped_wave', default=False, type=bool,
+            help='If True, plot fitted damped wave from stress oscillation')
+        parser.add_argument(
+            '-as', '--abs_wave', default=False, type=bool,
+            help='If True, plot fitted absolute wave from stress oscillation.')
+        parser.add_argument(
+            '-ph', '--plot_h', default=False, type=bool,
+            help='If True, plot the experimental height values.')
+        parser.add_argument(
+            '-pt', '--plot_t', default=False, type=bool,
+            help='If True, plot the experimental height values.')
+        parser.add_argument(
+            '-c1', '--colorax1', default='dodgerblue', type=str,
+            help='Left axis (stress) color.')
+        parser.add_argument(
+            '-c2', '--colorax2', default='silver', type=str,
+            help='Right axis (height) color.')
 
-    # Argumentos to DynamicCompression.total_plot
-    parser.add_argument(
-        '-ns', '--normal_wave', default=False, type=bool,
-        help='If True, plot fitted sine wave from height oscillation.')
-    parser.add_argument(
-        '-ds', '--damped_wave', default=False, type=bool,
-        help='If True, plot fitted damped wave from stress oscillation')
-    parser.add_argument(
-        '-as', '--abs_wave', default=False, type=bool,
-        help='If True, plot fitted absolute wave from stress oscillation.')
-    parser.add_argument(
-        '-ph', '--plot_h', default=False, type=bool,
-        help='If True, plot the experimental height values.')
-    parser.add_argument(
-        '-pt', '--plot_t', default=False, type=bool,
-        help='If True, plot the experimental height values.')
-    parser.add_argument(
-        '-c1', '--colorax1', default='dodgerblue', type=str,
-        help='Left axis (stress) color.')
-    parser.add_argument(
-        '-c2', '--colorax2', default='silver', type=str,
-        help='Right axis (height) color.')
+        # Arguments to file config
+        parser.add_argument(
+            '-n', '--file_name', default='total_plot.png', type=str,
+            help='File name to save the plot. Must specify the format (e.g. .png or .pdf).')
+        parser.add_argument(
+            '-d', '--file_dpi', default=300, type=int,
+            help='Figure resolution in dots per inch.')
+        parser.add_argument(
+            '-s', '--file_style', default='seaborn-v0_8-ticks', type=str,
+            help='Select oredefined matplotlib styles. '
+                 'The names of the available styles can be found in the list matplotlib.style.available.')
 
-    # Arguments to file config
-    parser.add_argument(
-        '-n', '--file_name', default='total_plot.png', type=str,
-        help='File name to save the plot. Must specify the format (e.g. .png or .pdf).')
-    parser.add_argument(
-        '-d', '--file_dpi', default=300, type=int,
-        help='Figure resolution in dots per inch.')
-    parser.add_argument(
-        '-s', '--file_style', default='seaborn-v0_8-ticks', type=str,
-        help='Select oredefined matplotlib styles. '
-             'The names of the available styles can be found in the list matplotlib.style.available.')
-
-    args = vars(parser.parse_args())
-    return tuple(args.values())
+        args = vars(parser.parse_args())
+        return tuple(args.values())
 
 
-args_values = argconfig()
+    args_values = argconfig()
 
-plt.style.use(args_values[12])
+    plt.style.use(args_values[12])
 
-data = DynamicCompression(
-    args_values[0],
-    args_values[1],
-    args_values[2],
-)
+    data = DynamicCompression(
+        args_values[0],
+        args_values[1],
+        args_values[2],
+    )
 
-fig = DynamicCompression.total_plot(
-    data,
-    args_values[3],
-    args_values[4],
-    args_values[5],
-    args_values[6],
-    args_values[7],
-    args_values[8],
-    args_values[9]
-)
-plt.show()
-fig.savefig(args_values[10], dpi=args_values[11])
-plt.close(fig)
+    fig = DynamicCompression.total_plot(
+        data,
+        args_values[3],
+        args_values[4],
+        args_values[5],
+        args_values[6],
+        args_values[7],
+        args_values[8],
+        args_values[9]
+    )
+    plt.show()
+    fig.savefig(args_values[10], dpi=args_values[11])
+    plt.close(fig)
