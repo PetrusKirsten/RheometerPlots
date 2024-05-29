749aa91db5ce859cd99d6634f67a0a588339e4fa
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-05-29 20:32:47 +0200

    Tomando a GUI do pllugin do inv3 como base

--- app.py
+++ app.py
@@ -11,7 +11,9 @@ class MainWindow(wx.Frame):
         wx.Frame.__init__(self, parent, title=title, size=(200, 200))
         self.SetIcon(wx.Icon('Data/chart_icon.ico'))
 
-        self.control = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)x
+        self.main_sizer = wx.BoxSizer(wx.VERTICAL)
+
+        # self.control = wx.TextCtrl(self, style=wx.TE_PROCESS_ENTER)
 
         self.CreateStatusBar()  # A Statusbar in the bottom of the window
 
@@ -31,15 +33,32 @@ class MainWindow(wx.Frame):
         self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
         self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
 
+        # Robot coil trajectory variables
+        self.mainTraj_sizer = wx.StaticBoxSizer(
+            wx.VERTICAL, self,
+            'Robotic coil trajectory')
+
         # Use some sizers to see layout options
-        self.sizer = wx.BoxSizer(wx.VERTICAL)
-        self.sizer.Add(self.control, 1, wx.ALIGN_CENTER)
+        # self.sizer = wx.BoxSizer(wx.VERTICAL)
+        # self.sizer.Add(self.control, 1, wx.ALIGN_CENTER)
 
         # Layout sizers
         # self.SetSizer(self.sizer)
         # self.SetAutoLayout(1)
         # self.sizer.Fit(self)
         self.Show()
+
+    def init_gui(self):
+        # self.EmgVisGui()
+        # self.CoilTrajGui()
+
+        self.main_sizer.Add(
+            self.mainTraj_sizer, 1,
+            wx.ALL, 20)
+
+        self.SetSizer(self.main_sizer)
+        self.main_sizer.Fit(self)
+        self.Layout()
 
     def OnAbout(self, e):
         # Create a message dialog box
@@ -57,7 +76,7 @@ class MainWindow(wx.Frame):
             self.filename = dlg.GetFilename()
             self.dirname = dlg.GetDirectory()
             f = open(os.path.join(self.dirname, self.filename), 'r')
-            self.control.SetValue(f.read())
+            # self.control.SetValue(f.read())
             f.close()
         dlg.Destroy()
 
