43af35a95d6658b3eb009964124012affe81f82b
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-10-07 10:31:24 +0200

    final freq and stress sweeps code and figures

--- custom plots/Plot-FreqSweeps.py
+++ custom plots/Plot-FreqSweeps.py
@@ -89,34 +89,15 @@ def columnRead(dataframe, key):
     return dataframe[key].to_numpy()
 
 
-def dataFreqSweep(dataframe, recovery=False):
-    freq = columnRead(dataframe, 'f in Hz')
-    gPrime, gDouble = columnRead(dataframe, "G' in Pa"), columnRead(dataframe, "G'' in Pa")
-    gPrime, gDouble = gPrime[gPrime > 0], gDouble[gDouble > 0]
+def dataFreqSweep(dataframe):
+    (freq,
+     gPrime,
+     gDouble) = (columnRead(dataframe, 'f in Hz'),
+                 columnRead(dataframe, "G' in Pa"),
+                 columnRead(dataframe, "G'' in Pa"))
+    freq, gPrime, gDouble = freq[freq > 0], gPrime[gPrime > 0], gDouble[gDouble > 0]
 
-    nG = gPrime.shape[0] // 2
-    nT = dataframe.index[dataframe['Seg'] == '2-1'].to_list()[0]
-
-    if recovery:
-        nT = dataframe.index[dataframe['Seg'] == '5-1'].to_list()[0]
-        freq, gPrime, gDouble = freq[nT:nT + nG], gPrime[nG:], gDouble[nG:]
-
-        return freq, gPrime, gDouble, constantMean(gPrime), constantMean(gDouble)
-
-    elif not recovery:
-        freq, gPrime, gDouble = freq[nT:nT + nG], gPrime[:nG], gDouble[:nG]
-        return freq, gPrime, gDouble, constantMean(gPrime), constantMean(gDouble)
-
-
-def dataFlow(dataframe):
-    time = columnRead(dataframe, 't in s')
-    shearRate, shearStress = columnRead(dataframe, "GP in 1/s"), columnRead(dataframe, "Tau in Pa")
-    segInd1 = dataframe.index[dataframe['Seg'] == '3-1'].to_list()[0]
-    segInd2 = dataframe.index[dataframe['Seg'] == '5-1'].to_list()[0]
-    timeSeg, shearRate, shearStress = time[segInd1: segInd2], shearRate[segInd1: segInd2], shearStress[segInd1: segInd2]
-    timeSeg = timeSeg - timeSeg[0]
-
-    return timeSeg, shearRate, shearStress
+    return freq, gPrime, gDouble, constantMean(gPrime), constantMean(gDouble)
 
 
 def plotModuli(
@@ -131,7 +112,7 @@ def plotModuli(
 
     ax.set_xlabel('Frequency (Hz)')
     # ax.set_xticks([])
-    ax.set_xlim([x[0], x[-1] + 20])
+    ax.set_xlim([x[0], x[-1] + 100])
     ax.set_xscale('log')
 
     ax.set_ylabel('Storage and loss moduli (Pa)')
@@ -154,55 +135,6 @@ def plotModuli(
     legendLabel(ax)
 
 
-def plotFlow(
-        ax, x, y,
-        axTitle, textSize, curveColor,
-        cteShear,  # in seg
-        yFlowLim=(0, 600)
-
-):
-    """Plots the Flow Shearing Assay."""
-    indexCteShear = np.where(x >= cteShear)[0][0]  # find the index where time >= final cte shear
-    timeCteShear, timeDecShear = np.split(x, [indexCteShear + 1])
-
-    ax.set_title(axTitle, size=10, color='crimson')
-    ax.spines[['top', 'bottom', 'left', 'right']].set_linewidth(0.75)
-
-    ax.set_xlabel('Time')
-    ax.set_xticks([cteShear])
-    ax.set_xticklabels([f'{cteShear} s'])
-    ax.set_xlim([x[0] - 10, x[-1] + 10])
-
-    ax.set_ylabel('Shear stress (Pa)')
-    # ax.set_yticks([])
-    ax.set_ylim(yFlowLim)
-    # ax.set_yscale('log')
-
-    # Plot cte strain rate rectangle/line
-    textCoord, textLabel = ((np.median(timeCteShear), 5),
-                            'Constant strain rate\n$\dot{γ}=300 \,\,\, s^{-1}$')
-    rectConfig = [(x[0] - cteShear, 0), 2*cteShear, yFlowLim[-1] + 50]
-    ax.text(textCoord[0], textCoord[1], textLabel,
-            horizontalalignment='center', verticalalignment='bottom',
-            color='k', size=textSize)
-    rect = Rectangle(*rectConfig, linewidth=1, linestyle='--', edgecolor='grey', facecolor='w',
-                     alpha=0.7, zorder=1)
-    ax.add_patch(rect)
-
-    # Plot decreasing strain rate text
-    textCoord, textLabel = ((np.median(timeDecShear), 5),
-                            'Step decrease of strain rate\n$300< \dot{γ} < 0.1 \,\,\, s^{-1}$')
-    ax.text(textCoord[0], textCoord[1], textLabel,
-            horizontalalignment='center', verticalalignment='bottom',
-            color='k', size=textSize)
-
-    # Plot data
-    ax.scatter(x, y, alpha=0.6,
-               lw=.5, c=curveColor, edgecolor='k', s=30, marker='o',
-               zorder=3)
-    # legendLabel(ax)
-
-
 def legendLabel(ax):
     """Applies consistent styling to legends in plots."""
     legend = ax.legend(frameon=True, framealpha=0.9, fancybox=False, scatterpoints=3)
@@ -219,8 +151,8 @@ def mainPlot(dataPath, color):
     fonts('C:/Users/petrus.kirsten/AppData/Local/Microsoft/Windows/Fonts/')
     markerSize = 50
     plt.style.use('seaborn-v0_8-ticks')
-    fig, axes = plt.subplots(figsize=(18, 6), facecolor='w', ncols=3)
-    fig.suptitle(f'Rheometry assay protocol to evaluate viscoelastic recovery | {sampleName}\n\n')
+    fig, axes = plt.subplots(figsize=(8, 6), facecolor='w', ncols=1)
+    fig.suptitle(f'Rheometry assay protocol to evaluate linear viscoelastic region | {sampleName}')
 
     # Common parameters
     text_coord = (0, 100)
@@ -234,39 +166,25 @@ def mainPlot(dataPath, color):
     # Plot 1: Oscillatory Frequency Sweep Assay
     timeAx1, storage, loss, storageMean, lossMean = dataFreqSweep(df)
     plotModuli(
-        axes[0], timeAx1, storage, loss, storageMean, lossMean, markerSize,
-        axTitle='Oscillatory frequency sweep assay.', curveColor=color,
+        axes, timeAx1, storage, loss, storageMean, lossMean, markerSize,
+        axTitle='', curveColor=color,
         textConfig=text_properties, textLabel=text_label, textCoord=text_coord, rectConfig=rect_properties,
         showRest=False)
 
-    # Plot 2: Flow Shearing Assay
-    timeAx2, rate, stress = dataFlow(df)
-    plotFlow(
-        axes[1], timeAx2, stress,
-        axTitle='Shear flow assay.', textSize=9.2, curveColor=color,
-        cteShear=300)
-
-    # Plot 3: Oscillatory Frequency Sweep Assay Again
-    timeAx3, storage, loss, storageMean, lossMean = dataFreqSweep(df, True)
-    plotModuli(
-        axes[2], timeAx3, storage, loss, storageMean, lossMean, markerSize,
-        'Oscillatory frequency sweep assay again.', curveColor=color,
-        textConfig=text_properties, textLabel=text_label, textCoord=text_coord, rectConfig=rect_properties,
-        showRest=False)
-
-    plt.subplots_adjust(wspace=0.175, top=0.890, bottom=0.14, left=0.05, right=0.95)
+    # plt.subplots_adjust(wspace=0.175, top=0.890, bottom=0.14, left=0.05, right=0.95)
+    plt.tight_layout()
     fig.savefig(dirSave, facecolor='w', dpi=600)
     plt.show()
 
 
 # Run the main plotting function
 
-filePath = ('C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt_iCar/10pct_0WSt_iCar'
-            '-RecoveryAndFlow_4.xlsx')  # -> CEBB PC
+filePath = ('C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/5pct/'
+            'FreqSweep.xlsx')  # -> CEBB PC
 # filePath = ('C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
 #             '-RecoveryAndFlow_2.xlsx')  # -> CEBB PC
 
 # filePath = ('C:/Users/Petrus Kirsten/Documents/GitHub/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
 #             '-RecoveryAndFlow_2.xlsx')  # personal PC
 
-mainPlot(dataPath=filePath, color='orange')
+mainPlot(dataPath=filePath, color='coral')
