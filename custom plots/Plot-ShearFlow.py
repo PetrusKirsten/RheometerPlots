ca6e1a72990613fe4a3bc69b4596408367cc9d0f
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-10-10 15:33:54 +0200

    updates

--- custom plots/Plot-ShearFlow.py
+++ custom plots/Plot-ShearFlow.py
@@ -1,10 +1,9 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
 from matplotlib.font_manager import FontProperties
-from matplotlib.patches import Rectangle
-from scipy.optimize import curve_fit
 
 
 def fonts(folder_path, s=11, m=13):
@@ -88,91 +87,112 @@ def getCteMean(values, tolerance=100):
 
 
 def columnsRead(dataframe):
-    time, shearRate, shearStress = (
+    time, shearRate, shearStress, viscosity = (
         dataframe['t in s'].to_numpy(),
-        dataframe["GP in 1/s"].to_numpy(),
-        dataframe["Tau in Pa"].to_numpy())
+        dataframe['ɣ̇ in 1/s'].to_numpy(),
+        dataframe['τ in Pa'].to_numpy(),
+        dataframe['η in mPas'].to_numpy())
 
-    seg3, seg4, seg5 = (dataframe.index[dataframe['Seg'] == '3-1'].to_list()[0],
-                        dataframe.index[dataframe['Seg'] == '4-1'].to_list()[0],
-                        dataframe.index[dataframe['Seg'] == '5-1'].to_list()[0])
+    seg3, seg4, seg5 = (dataframe.index[dataframe['SegIndex'] == '3|1'].to_list()[0],
+                        dataframe.index[dataframe['SegIndex'] == '4|1'].to_list()[0],
+                        dataframe.index[dataframe['SegIndex'] == '5|1'].to_list()[0])
 
     tCte, tSteps = time[seg3:seg4], time[seg4:seg5]
     shearRate_cte, shearRate_steps = shearRate[seg3:seg4], shearRate[seg4:seg5]
     shearStress_cte, shearStress_steps = shearStress[seg3:seg4], shearStress[seg4:seg5]
+    viscosity_cte, viscosity_steps = viscosity[seg3:seg4], viscosity[seg4:seg5]
 
     return ([tCte - tCte[0], tSteps - tCte[0]],
             [shearRate_cte, shearRate_steps],
-            [shearStress_cte, shearStress_steps])
+            [shearStress_cte, shearStress_steps],
+            [viscosity_cte, viscosity_steps])
 
 
 def getSamplesData(dataPath, nSt, nIc):
     dict_cteRate, dict_stepsRate = {}, {}  # Dicionário para armazenar resultados por caminho de arquivo
-    st_time, st_rateCte, st_rateSteps, st_stressCte, st_stressSteps = [], [], [], [], []
-    ic_time, ic_rateCte, ic_rateSteps, ic_stressCte, ic_stressSteps = [], [], [], [], []
+    st_time, st_rateCte, st_rateSteps, st_stressCte, st_stressSteps, st_viscosityCte, st_viscositySteps = [], [], [], [], [], [], []
+    ic_time, ic_rateCte, ic_rateSteps, ic_stressCte, ic_stressSteps, ic_viscosityCte, ic_viscositySteps = [], [], [], [], [], [], []
 
     for sample, path in enumerate(dataPath):
         df = pd.read_excel(path)
 
         if sample < nSt:
-            st_time_i, st_rate_i, st_stress_i = columnsRead(df)
+            st_time_i, st_rate_i, st_stress_i, st_viscosity_i = columnsRead(df)
 
-            (st_time.append(st_time_i[0]), st_rateCte.append(st_rate_i[0]), st_rateSteps.append(st_rate_i[1]),
-             st_stressCte.append(st_stress_i[0]), st_stressSteps.append(st_stress_i[1]))
+            st_time.append(st_time_i[0])
+
+            st_rateCte.append(st_rate_i[0])
+            st_rateSteps.append(st_rate_i[1])
+
+            st_stressCte.append(st_stress_i[0])
+            st_stressSteps.append(st_stress_i[1])
+
+            st_viscosityCte.append(st_viscosity_i[0])
+            st_viscositySteps.append(st_viscosity_i[1])
 
         else:
-            ic_time_i, ic_rate_i, ic_stress_i = columnsRead(df)
+            ic_time_i, ic_rate_i, ic_stress_i, ic_viscosity_i = columnsRead(df)
 
-            if sample == 3:
-                (ic_time.append(ic_time_i[0][::2]), ic_rateCte.append(ic_rate_i[0][::2]),
-                 ic_rateSteps.append(ic_rate_i[1][::2]), ic_stressCte.append(ic_stress_i[0][::2]),
-                 ic_stressSteps.append(ic_stress_i[1][::2]))
-            else:
-                (ic_time.append(ic_time_i[0]), ic_rateCte.append(ic_rate_i[0]), ic_rateSteps.append(ic_rate_i[1]),
-                 ic_stressCte.append(ic_stress_i[0]), ic_stressSteps.append(ic_stress_i[1]))
+            ic_time.append(ic_time_i[0])
+
+            ic_rateCte.append(ic_rate_i[0])
+            ic_rateSteps.append(ic_rate_i[1])
+
+            ic_stressCte.append(ic_stress_i[0])
+            ic_stressSteps.append(ic_stress_i[1])
+
+            ic_viscosityCte.append(ic_viscosity_i[0])
+            ic_viscositySteps.append(ic_viscosity_i[1])
 
     # Cte shear rate data
     #   Pure starch
     (dict_cteRate[f'st_time'],
      dict_cteRate[f'st_rateCte'],
-     dict_cteRate[f'st_stressCte']) = st_time, st_rateCte, st_stressCte
+     dict_cteRate[f'st_stressCte'],
+     dict_cteRate[f'st_viscosityCte']) = st_time, st_rateCte, st_stressCte, st_viscosityCte
+
     (dict_stepsRate[f'st_rateSteps'],
-     dict_stepsRate[f'st_stressSteps']) = st_rateSteps, st_stressSteps
+     dict_stepsRate[f'st_stressSteps'],
+     dict_stepsRate[f'st_viscositySteps']) = st_rateSteps, st_stressSteps, st_viscositySteps
 
     #   Starch + iCar
     (dict_cteRate[f'ic_time'],
      dict_cteRate[f'ic_rateCte'],
-     dict_cteRate[f'ic_stressCte']) = ic_time, ic_rateCte, ic_stressCte
+     dict_cteRate[f'ic_stressCte'],
+     dict_cteRate[f'ic_viscosityCte']) = ic_time, ic_rateCte, ic_stressCte, ic_viscosityCte
+
     (dict_stepsRate[f'ic_rateSteps'],
-     dict_stepsRate[f'ic_stressSteps']) = ic_rateSteps, ic_stressSteps
+     dict_stepsRate[f'ic_stressSteps'],
+     dict_stepsRate[f'ic_viscositySteps']) = ic_rateSteps, ic_stressSteps, ic_viscositySteps
 
     return dict_cteRate, dict_stepsRate
 
 
 def plotFlowTime(
-        ax, x, y, yErr,
-        axTitle, curveColor, sampleName,
+        ax, x, y,
+        axTitle, yLabel, yLim,
+        curveColor, markerStyle,
+        sampleName,
         logScale=False,
-        fit=False,
-        yFlowLim=(125, 475)):
+        fit=False):
     ax.set_title(axTitle, size=9, color='crimson')
     ax.spines[['top', 'bottom', 'left', 'right']].set_linewidth(0.75)
     ax.set_xlabel('Time (s)')
     ax.set_xscale('log' if logScale else 'linear')
-    ax.set_xlim([-30, +850])
+    ax.set_xlim([-65, +1650])
 
-    ax.set_ylabel('Shear stress (Pa)')
+    ax.set_ylabel(f'{yLabel}')
     ax.set_yscale('log' if logScale else 'linear')
-    ax.set_ylim(yFlowLim)
+    ax.set_ylim(yLim)
 
     if fit:
         ax.plot(
-            x, y, color=curveColor, linestyle='-', linewidth=1,
-            label=sampleName, zorder=2)
+            x, y, color=curveColor, linestyle=':', linewidth=1,
+            zorder=2)
     else:
         ax.errorbar(
-            x, y, yerr=0, color=curveColor, alpha=1,
-            fmt='o', markersize=7, mec='k', mew=0.5,
+            x[::3], y[::3], yerr=0, color=curveColor, alpha=0.8,
+            fmt=markerStyle, markersize=7, mec='k', mew=0.5,
             capsize=3, lw=1, linestyle='',  # ecolor='k'
             label=sampleName, zorder=3)
 
@@ -194,68 +214,86 @@ def main(dataPath):
     dirSave = f'{Path(filePath[0]).parent}' + f'\\{fileName}' + '.png'
 
     plt.style.use('seaborn-v0_8-ticks')
-    fig, axes = plt.subplots(figsize=(10, 6), facecolor='w', ncols=1)
+    fig, axStress = plt.subplots(figsize=(10, 7), facecolor='w', ncols=1)
+    axVisc = axStress.twinx()
     fig.suptitle(f'Shear flow')
 
     constantShear, _ = getSamplesData(dataPath, 2, 3)
 
-    x_st, y_st, x_ic, y_ic = (
+    x_st, s_st, v_st, x_ic, s_ic, v_ic = (
         constantShear['st_time'],
         constantShear['st_stressCte'],
+        constantShear['st_viscosityCte'],
         #
         constantShear['ic_time'],
-        constantShear['ic_stressCte'])
+        constantShear['ic_stressCte'],
+        constantShear['ic_viscosityCte'])
+
     tableRows = []
-    for curve in range(1):
-        st_params, st_covariance = curve_fit(powerLaw, x_st[curve], y_st[curve], p0=(2, 1, 0))
+    for curve in range(2):
+        st_params, st_covariance = curve_fit(powerLaw, x_st[curve], s_st[curve], p0=(2, 1, 0))
         st_errors = np.sqrt(np.diag(st_covariance))
         st_K, st_n, st_sigmaZero = st_params
-        x_fit = np.linspace(0, 600, 600)
+        x_fit = np.linspace(0, 700, 700)
         y_fit = powerLaw(x_fit, st_K, st_n, st_sigmaZero)
 
         plotFlowTime(
-            axes, x_fit.tolist(), y_fit.tolist(), yErr=0,
-            axTitle='', curveColor='orange',
-            sampleName=f'Fit 10%_0WSt_{curve + 1}',
+            axStress, x_fit.tolist(), y_fit.tolist(),
+            axTitle='', yLabel='', yLim=(100, 600),
+            curveColor='silver', markerStyle=None,
+            sampleName=f'',
             fit=True)
         plotFlowTime(
-            axes, x_st[curve].tolist(), y_st[curve], yErr=0,
-            axTitle='', curveColor='orange',
+            axStress, x_st[curve].tolist(), s_st[curve],
+            axTitle='', yLabel='Shear stress (Pa)', yLim=(100, 600),
+            curveColor='silver', markerStyle='o',
             sampleName=f'10%_0WSt_{curve + 1}')
+        plotFlowTime(
+            axVisc, (x_st[curve] + 800).tolist(), v_st[curve],
+            axTitle='', yLabel='Viscosity (mPa·s)', yLim=(400, 2000),
+            curveColor='silver', markerStyle='s',
+            sampleName=f'')
 
         tableRows = exportFit(
             f'10%_0WSt_{curve + 1}',
             st_K, st_n, st_sigmaZero, st_errors,
             tableRows)
         print(
-            f'\n10_0WSt thixotropy fit parameters:\n\n'
+            f'\n10_0WSt thixotropy fit parameters:\n'
             f'K = {st_K:.2f} ± {st_errors[0]:.2f},\n'
             f'n = {st_n:.2f} ± {st_errors[1]:.2f},\n'
             f'sigma_0 = {st_sigmaZero:.1f} ± {st_errors[2]:.2f}\n')
 
-    for curve in range(2):
-        ic_params, ic_covariance = curve_fit(powerLaw, x_ic[curve], y_ic[curve])
+    for curve in range(3):
+        ic_params, ic_covariance = curve_fit(powerLaw, x_ic[curve], s_ic[curve])
         ic_errors = np.sqrt(np.diag(ic_covariance))
         ic_K, ic_n, ic_sigmaZero = ic_params
-        x_fit = np.linspace(0, 600, 600)
+        x_fit = np.linspace(0, 700, 700)
         y_fit = powerLaw(x_fit, ic_K, ic_n, ic_sigmaZero)
 
         plotFlowTime(
-            axes, x_fit.tolist(), y_fit.tolist(), yErr=0,
-            axTitle='', curveColor='dodgerblue',
-            sampleName=f'Fit 10%_0WSt_iCar_{curve + 1}',
+            axStress, x_fit.tolist(), y_fit.tolist(),
+            axTitle='', yLabel='', yLim=(100, 600),
+            curveColor='deepskyblue', markerStyle=None,
+            sampleName=f'',
             fit=True)
         plotFlowTime(
-            axes, x_ic[curve].tolist(), y_ic[curve], yErr=0,
-            axTitle='', curveColor='dodgerblue',
+            axStress, x_ic[curve].tolist(), s_ic[curve],
+            axTitle='', yLabel='Shear stress (Pa)', yLim=(100, 600),
+            curveColor='deepskyblue', markerStyle='o',
             sampleName=f'10%_0WSt_iCar_{curve + 1}')
+        plotFlowTime(
+            axVisc, (x_ic[curve] + 800).tolist(), v_ic[curve],
+            axTitle='', yLabel='Viscosity (mPa·s)', yLim=(400, 2000),
+            curveColor='deepskyblue', markerStyle='s',
+            sampleName=f'')
 
         tableRows = exportFit(
             f'10%_0WSt_iCar_{curve + 1}',
             ic_K, ic_n, ic_sigmaZero, ic_errors,
             tableRows)
         print(
-            f'\n10_0WSt_iCar_{curve + 1} thixotropy fit parameters:\n\n'
+            f'\n10_0WSt_iCar_{curve + 1} thixotropy fit parameters:\n'
             f'K = {ic_K:.2f} ± {ic_errors[0]:.2f},\n'
             f'n = {ic_n:.2f} ± {ic_errors[1]:.2f},\n'
             f'sigma_0 = {ic_sigmaZero:.1f} ± {ic_errors[2]:.2f}\n')
@@ -269,24 +307,18 @@ def main(dataPath):
     plt.show()
 
 
-# Run the main plotting function
-filePath = [  # Pure WSt
-    'C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
-    '-RecoveryAndFlow_1.xlsx',
-    'C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
-    '-RecoveryAndFlow_2.xlsx',
-    # WSt + iCar
-    'C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt_iCar/10pct_0WSt_iCar'
-    '-RecoveryAndFlow_2.xlsx',
-    'C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt_iCar/10pct_0WSt_iCar'
-    '-RecoveryAndFlow_3.xlsx',
-    'C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt_iCar/10pct_0WSt_iCar'
-    '-RecoveryAndFlow_4.xlsx']  # -> CEBB PC
-
-# filePath = ('C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
-#             '-RecoveryAndFlow_2.xlsx')  # -> CEBB PC
-
-# filePath = ('C:/Users/Petrus Kirsten/Documents/GitHub/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
-#             '-RecoveryAndFlow_2.xlsx')  # personal PC
 if __name__ == '__main__':
+    # filePath = ('C:/Users/Petrus Kirsten/Documents/GitHub/RheometerPlots/data/031024/10pct_0WSt/10pct_0WSt'
+    #             '-RecoveryAndFlow_2.xlsx')  # personal PC
+
+    folderPath = "C:/Users/petrus.kirsten/PycharmProjects/RheometerPlots/data"
+    filePath = [
+        folderPath + "/031024/10_0WSt/10_0WSt-viscRec_1.xlsx",
+        folderPath + "/031024/10_0WSt/10_0WSt-viscRec_2.xlsx",
+        #
+        # folderPath + "10_0WSt_iCar/10_0WSt_iCar-viscoRecoveryandFlow_1.xlsx",
+        folderPath + "/031024/10_0WSt_iCar/10_0WSt_iCar-viscoRecoveryandFlow_2.xlsx",
+        folderPath + "/031024/10_0WSt_iCar/10_0WSt_iCar-viscoRecoveryandFlow_3.xlsx",
+        folderPath + "/031024/10_0WSt_iCar/10_0WSt_iCar-viscoRecoveryandFlow_4.xlsx"]
+
     main(dataPath=filePath)
