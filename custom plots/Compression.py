04b659ddeceaac0da3fb84120af36ea844ae0831
Petrus N Kirsten <petrus.kirsten@gmail.com>
2024-10-16 09:49:55 +0200

    add fitting to compression

--- custom plots/Compression.py
+++ custom plots/Compression.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from pathlib import Path
+
+from matplotlib.patches import Rectangle
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt, ticker
 from matplotlib.ticker import MultipleLocator
@@ -28,6 +30,16 @@ def fonts(folder_path, s=11, m=13):
     plt.rc('ytick', labelsize=s)  # fontsize of the tick labels
     plt.rc('legend', fontsize=s)  # legend fontsize
     plt.rc('figure', titlesize=m)  # fontsize of the figure title
+
+
+def fitLinear(strain, slope, intercept):
+    return slope*strain + intercept
+
+
+def arraySplit(xArr, yArr, startValue, endValue):
+    startIndex, endIndex = np.where(xArr >= startValue)[0][0], np.where(xArr <= endValue)[0][-1]
+
+    return xArr[startIndex:endIndex], yArr[startIndex:endIndex]
 
 
 def getSamplesData(dataPath, n5st=0, n10St=0, nIc=0, n5Kc=0, n10Kc=1):
@@ -74,10 +86,10 @@ def plotCompression(nSamples, sampleName,
                     ax, x, y,
                     axTitle, yLabel, yLim, xLabel, xLim, axisColor,
                     curveColor, markerStyle, markerFColor, markerEColor, markerEWidth=0.5,
-                    strain=False, lineStyle='', logScale=False):
+                    strain=False, linearFitting=False, logScale=False):
     def legendLabel():
         """Applies consistent styling to legends in plots."""
-        legend = ax.legend(fancybox=False, frameon=True, framealpha=0.9, fontsize=9)
+        legend = ax.legend(loc=4, fancybox=False, frameon=False, framealpha=0.9, fontsize=10)
         legend.get_frame().set_facecolor('w')
         legend.get_frame().set_edgecolor('whitesmoke')
 
@@ -101,22 +113,60 @@ def plotCompression(nSamples, sampleName,
 
         if strain:
             ax.plot(
-                x[0], y[0],
+                x, y,
                 color=curveColor, alpha=0.8, lw=1.5, linestyle=':',
                 label=f'{sampleName}', zorder=3)
 
+        elif linearFitting:
+            # configs to split the values at the linear elastic region
+            startVal, endVal = 6, 16
+            x_toFit, y_toFit = arraySplit(x, y, startVal, endVal)
+            (slope, intercept), covariance = curve_fit(fitLinear, x_toFit, y_toFit)  # p0=(y_mean[0], y_mean[-1], 100))
+            (slopeErr, interceptErr) = np.sqrt(np.diag(covariance))
+
+            xFit = np.linspace(startVal, endVal, 100)
+            yFit = fitLinear(xFit, slope, intercept)
+            ax.plot(
+                xFit, yFit,
+                color='crimson', alpha=0.8, lw=1.25, linestyle='-',
+                label=f'Linear fitting', zorder=4)
+
+            # show text and rectangle at the linear region
+            textLabel, textCoord = 'Linear elastic region', (xFit[-1] + 2, np.median(yFit))
+            textConfig = {'horizontalalignment': 'left', 'verticalalignment': 'top', 'color': 'crimson', 'size': 10}
+            rectConfig = [(xFit[0], 0), xFit[-1] - xFit[0], yFit[-1] + 2]
+            ax.text(textCoord[0], textCoord[1], s=textLabel, **textConfig)
+
+            textLabel, textCoord = (
+                f'YM = ${slope*100:.1f}$ $±$ ${slopeErr*100:.1f}$ $Pa$', (xFit[-1] + 1, np.median(yFit) - 20))
+            textConfig = {'horizontalalignment': 'left', 'verticalalignment': 'top', 'color': 'k', 'size': 9}
+            ax.text(textCoord[0], textCoord[1], s=textLabel, **textConfig)
+
+            textLabel, textCoord = f'{startVal}%', (xFit[0] + 0.5, 3)
+            textConfig = {'horizontalalignment': 'left', 'verticalalignment': 'bottom', 'color': 'k', 'size': 8}
+            ax.text(textCoord[0], textCoord[1], s=textLabel, **textConfig, zorder=5)
+
+            textLabel, textCoord = f'{endVal}%', (xFit[-1] - 0.5, 3)
+            textConfig = {'horizontalalignment': 'right', 'verticalalignment': 'bottom', 'color': 'k', 'size': 8}
+            ax.text(textCoord[0], textCoord[1], s=textLabel, **textConfig, zorder=5)
+
+            rect = Rectangle(
+                *rectConfig, linewidth=1, edgecolor='w', facecolor='crimson', alpha=0.1, zorder=1)
+            ax.add_patch(rect)
+
         else:
             ax.errorbar(
-                x[0], y, 0,
-                color=curveColor, alpha=0.35,
+                x, y, 0,
+                color=curveColor, alpha=0.85,
                 fmt=markerStyle, markersize=6, mfc=markerFColor, mec=markerEColor, mew=markerEWidth,
-                capsize=0, lw=1, linestyle=lineStyle,
+                capsize=0, lw=1, linestyle='',
                 label=f'{sampleName}',
-                zorder=4)
+                zorder=3)
 
     # x = np.mean(x, axis=0)
     # yerr = np.std(y, axis=0)
     # y = np.mean(y, axis=0)
+    legendLabel()
     configPlot()
 
 
@@ -127,18 +177,19 @@ def main(dataPath):
     fileName = '10pct_0WSt_and_Car-CompressionToBreakage'
     dirSave = Path(*Path(filePath[0]).parts[:Path(filePath[0]).parts.index('data') + 1])
 
-    fig, axForce = plt.subplots(figsize=(6, 8), facecolor='w', ncols=1)
+    fig, axForce = plt.subplots(figsize=(8, 8), facecolor='w', ncols=1)
 
     plt.style.use('seaborn-v0_8-ticks')
     fig.suptitle(f'Compression modulus')
-    forceColor, strainColor = 'tomato', 'mediumseagreen'
+    forceColor, strainColor = 'whitesmoke', 'mediumseagreen'
 
     st5_nSamples, st10_nSamples, ic10_nSamples, kc10_nSamples = 0, 0, 0, 1
     data = getSamplesData(dataPath)
 
-    f_10kc, s_10kc = (
-        data['10% WSt kCar force to break'],
-        data['10% WSt kCar height to break'])
+    force_10kc, strain_10kc = (
+        data['10% WSt kCar force to break'][0],
+        data['10% WSt kCar height to break'][0])
+    stress_10kc = force_10kc / (30/1000)
 
     (fTitle, fLimits,
      hTitle, hLimits) = (
@@ -147,15 +198,21 @@ def main(dataPath):
 
     plotCompression(
         nSamples=kc10_nSamples,
-        ax=axForce, x=s_10kc, y=f_10kc[0] / (30/1000), axisColor='k',
+        ax=axForce, x=strain_10kc, y=stress_10kc, axisColor='k',
         axTitle='', yLabel=fTitle, yLim=fLimits, xLabel=hTitle, xLim=hLimits,
         curveColor=forceColor, markerStyle='o', markerFColor=forceColor, markerEColor='k',
-        sampleName=f'')
+        linearFitting=True, sampleName=f'')
 
+    plotCompression(
+        nSamples=kc10_nSamples,
+        ax=axForce, x=strain_10kc, y=stress_10kc, axisColor='k',
+        axTitle='', yLabel=fTitle, yLim=fLimits, xLabel=hTitle, xLim=hLimits,
+        curveColor=forceColor, markerStyle='o', markerFColor=forceColor, markerEColor='k',
+        sampleName=f'Test')
     # axForce.spines['left'].set_color(forceColor)
     # axForce.spines['right'].set_color(strainColor)
 
-    plt.subplots_adjust(wspace=0.175, top=0.940, bottom=0.08, left=0.12, right=0.96)
+    plt.subplots_adjust(wspace=0.175, top=0.940, bottom=0.08, left=0.09, right=0.96)
     # plt.tight_layout()
     plt.show()
     fig.savefig(f'{dirSave}' + f'\\{fileName}' + '.png', facecolor='w', dpi=600)
